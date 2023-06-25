9b3f52dae85dd5a9b8452edcd0353b6959eaf0db
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-06-26 00:34:49 +0300

    completed lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :title_not_clickbait
+
+    private
+  
+    def title_not_clickbait
+        clickbait_phrases = ["clickbait", "sensational"]
+        forbidden_title = "True Facts"
+      
+        if title.present? && clickbait_phrases.any? { |phrase| title.downcase.include?(phrase) }
+          errors.add(:title, "is clickbait")
+        elsif title == forbidden_title
+          errors.add(:title, "is not allowed")
+        end
+    end
 end
